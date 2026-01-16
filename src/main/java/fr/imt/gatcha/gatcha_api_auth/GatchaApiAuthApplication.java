bbc3b2e03ed257a714b820b111d1c4c9cf6b1075
Julien <julienbouin31@gmail.com>
2026-01-16 11:02:04 +0100

    add basic spring structure

---
+++ src/main/java/fr/imt/gatcha/gatcha_api_auth/GatchaApiAuthApplication.java
@@ -0,0 +1,13 @@
+package fr.imt.gatcha.gatcha_api_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatchaApiAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatchaApiAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/imt/gatcha/gatcha_api_auth/GatchaApiAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.imt.gatcha.gatcha_api_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatchaApiAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
